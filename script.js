39610bd4e76205e81c1bb9190b82c5726def5213
Dawn Olivier <dmarieolivier@gmail.com>
2022-03-29 20:36:08 -0400

    add script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const h2 = document.createElement("h2");
+/*hide const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2); */
 
-const h3 = document.createElement("h3");
+/* const h3 = document.createElement("h3");
 h3.textContent = "I am a third level Header";
 h3.style.fontFamily="Myriad Pro";
 h3.style.textTransform="Uppercase";
 h3.style.color="lightblue";
-document.querySelector("body").appendChild(h3);
+document.querySelector("body").appendChild(h3); */
