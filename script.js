7c96dd518b609c133a4429bb5714af3c92074007
Dawn Olivier <dmarieolivier@gmail.com>
2022-03-29 22:23:13 -0400

    added font awesome, javascript, and fonts; started working on header

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-/*hide const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2); */
+// date and time 
+var today = new Date();
+var date = today.getMonth()+1 + '-' +today.getDate() + '-' +today.getFullYear();
 
-/* const h3 = document.createElement("h3");
-h3.textContent = "I am a third level Header";
-h3.style.fontFamily="Myriad Pro";
-h3.style.textTransform="Uppercase";
-h3.style.color="lightblue";
-document.querySelector("body").appendChild(h3); */
+var today = new Date();
+
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+var dateTime = date+' | '+time;
+
+document.getElementById("p1").innerHTML = dateTime;
